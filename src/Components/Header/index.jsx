8e02dcac0b8d2dc844f2d5c30a0db07d7f7cc02f
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2025-08-04 21:01:07 -0300

    ajustes

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -15,17 +15,16 @@ export default function Header({ filtro, setFiltro, categoriaSelecionada, setCat
       </button>
 
       <div className="flex gap-4 justify-center items-center w-full">
-        <h1 className="text-[15px] lg:text-2xl whitespace-nowrap">RM Eletrodoméstico</h1>
+        <h1 className="ml-5 text-[15px] lg:text-2xl whitespace-nowrap">RM Eletrodoméstico</h1>
 
         <input
           type="text"
           value={filtro}
           onChange={(e) => setFiltro(e.target.value)}
-          className="bg-white p-2 text-sm text-black placeholder-gray-700 rounded focus:outline-none w-[250px] sm:w-[350px] md:w-[450px]"
+          className="bg-white p-2 text-sm text-black placeholder-gray-700 rounded focus:outline-none w-[200px] sm:w-[350px] md:w-[450px]"
           placeholder="Olá, o que você procura?"
         />
 
-        {/* Select visível só em desktop */}
         <select
           className="hidden lg:block bg-white text-gray-700 p-2 rounded focus:outline-none cursor-pointer"
           value={categoriaSelecionada}
@@ -41,12 +40,10 @@ export default function Header({ filtro, setFiltro, categoriaSelecionada, setCat
         </select>
       </div>
 
-      {/* Menu mobile */}
       {menuOpen && (
         <div className="absolute top-20 left-0 w-64 h-screen bg-[#3b3360] shadow-lg z-50 p-4 flex flex-col gap-4">
           <h2 className="text-xl mb-2">Categorias</h2>
 
-          {/* Select visível só em mobile */}
           <select
             className="block lg:hidden bg-white text-gray-700 p-2 rounded focus:outline-none cursor-pointer"
             value={categoriaSelecionada}
